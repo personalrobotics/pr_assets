bb4b8cdc5232c5187b7e16fdc9892f60ff90f7ef
Youngsun Kim <yskim041@gmail.com>
2017-04-07 18:40:49 -0400

    update converter and URDFs

--- convert_kinbody_to_urdf.py
+++ convert_kinbody_to_urdf.py
@@ -25,12 +25,32 @@ class Kinbody_Converter:
         return True
 
     # TODO:
-    # handle Quat, RotationAxis
-    # handle sphere with Render and 0 radius
+    # Body - Geom - Quat, RotationAxis, RotationMat, Vertices
+    # Translation
+    # Joint
+    # Mass
+    # Kinbody (load other kinbody file)
+    # orcdchomp support?
     def convert_child(self, parent, level=0):
-        # print(level, parent.tag, parent.attrib, parent.text.strip())
-        if parent.tag.lower() == 'geom':
+        print(level, parent.tag, parent.attrib, parent.text.strip())
+        if (parent.tag.lower() == 'body') and (self.link is None):
+            robot = self.urdf.iter('robot').next()
+            link_name = parent.attrib['name'] if 'name' in parent.attrib else 'base_link'
+            self.link = self.add_sub_element(robot, 'link',
+                                             {'name': link_name})
+            for child in parent:
+                self.convert_child(child, level + 1)
+
+            if len(self.link.getchildren()) == 0:
+                robot.remove(self.link)
+                self.link = None
+
+        elif parent.tag.lower() == 'geom':
             gtype = parent.attrib['type']
+            if 'render' in parent.attrib:
+                is_render = parent.attrib['render']
+            else:
+                is_render = 'true'
             features = dict()
             for child in parent:
                 if child.tag.lower() == 'extents':
@@ -42,32 +62,11 @@ class Kinbody_Converter:
                     features[child.tag.lower()] = extents_str.strip()
                 else:
                     features[child.tag.lower()] = child.text.strip()
+            if 'translation' not in features:
+                features['translation'] = '0 0 0'
 
-            collision = self.add_sub_element(self.link, 'collision')
-            visual = self.add_sub_element(self.link, 'visual')
+            self.add_geom(self.link, features, gtype, is_render)
 
-            if gtype == 'trimesh':
-                self.add_mesh_data(collision, features['data'])
-                self.add_mesh_render(visual, features['render'])
-
-            else:
-                self.add_origin(collision, features['translation'])
-                self.add_origin(visual, features['translation'])
-                self.add_color(visual, features)
-
-                if gtype == 'box':
-                    self.add_box(collision, features['extents'])
-                    self.add_box(visual, features['extents'])
-                elif gtype == 'cylinder':
-                    self.add_cylinder(collision,
-                                      features['radius'],
-                                      features['height'])
-                    self.add_cylinder(visual,
-                                      features['radius'],
-                                      features['height'])
-                elif gtype == 'sphere':
-                    self.add_sphere(collision, features['radius'])
-                    self.add_sphere(visual, features['radius'])
         elif parent.tag.lower() == 'orcdchomp':
             return
         else:
@@ -82,6 +81,45 @@ class Kinbody_Converter:
 
     def add_origin(self, parent, val):
         self.add_sub_element(parent, 'origin', {'xyz': val})
+
+    def add_geom(self, parent, features, gtype, is_render='true'):
+        collision = self.add_sub_element(parent, 'collision')
+        visual = self.add_sub_element(parent, 'visual')
+        if gtype == 'trimesh':
+            self.add_mesh_data(collision, features['data'])
+            if 'render' in features:
+                self.add_mesh_render(visual, features['render'])
+            else:
+                parent.remove(visual)
+        elif gtype == 'sphere':
+            if 'render' in features:
+                self.add_mesh_render(visual, features['render'])
+                if ('diffusecolor' in features) or ('ambientColor' in features):
+                    self.add_color(visual, features)
+                parent.remove(collision)
+            else:
+                self.add_origin(collision, features['translation'])
+                self.add_origin(visual, features['translation'])
+                self.add_color(visual, features)
+                self.add_sphere(collision, features['radius'])
+                self.add_sphere(visual, features['radius'])
+        else:
+            self.add_origin(collision, features['translation'])
+            self.add_origin(visual, features['translation'])
+            self.add_color(visual, features)
+            if gtype == 'box':
+                self.add_box(collision, features['extents'])
+                self.add_box(visual, features['extents'])
+            elif gtype == 'cylinder':
+                self.add_cylinder(collision,
+                                  features['radius'],
+                                  features['height'])
+                self.add_cylinder(visual,
+                                  features['radius'],
+                                  features['height'])
+
+        if is_render == 'false':
+            parent.remove(visual)
 
     def add_box(self, parent, val):
         geo = self.add_sub_element(parent, 'geometry')
@@ -105,9 +143,10 @@ class Kinbody_Converter:
         if len(ws) == 1:
             self.add_sub_element(geo, 'mesh', {'filename': ws[0]})
         else:
+            scale = float(ws[1])
             self.add_sub_element(geo, 'mesh',
                                  {'filename': ws[0],
-                                  'scale': '%.2f %.2f %.2f' % (ws[1], ws[1], ws[1])})
+                                  'scale': '%.2f %.2f %.2f' % (scale, scale, scale)})
 
     def add_color(self, parent, features):
         if 'diffusecolor' in features:
@@ -115,7 +154,7 @@ class Kinbody_Converter:
         elif 'ambientcolor' in features:
             val = features['ambientcolor']
         else:
-            val = '0.2 0.2 0.8'
+            return
 
         if 'transparency' in features:
             val += ' ' + features['transparency']
@@ -133,10 +172,8 @@ class Kinbody_Converter:
         urdf_root = ET.Element('robot')
         urdf_root.attrib = self.kinbody.getroot().attrib
         urdf_root.text = ''
-        self.link = self.add_sub_element(urdf_root, 'link',
-                                         {'name': 'base_link'})
-        self.convert_child(self.kinbody.getroot())
         self.urdf._setroot(urdf_root)
+        self.convert_child(self.kinbody.getroot())
         return self.urdf
 
     def print_tree(self, parent, level=0):
@@ -178,10 +215,15 @@ def script_main():
 
     indent(urdf.getroot())
 
-    newfilename = filename[:-12] + '.urdf'
+    newfilename = filename.replace('.kinbody', '').replace('.xml', '') + '.urdf'
     if os.path.isfile(newfilename):
+        backup_name = newfilename + '.back'
+        if os.path.isfile(backup_name):
+            from time import gmtime, strftime
+            backup_name = '%s_%s.back' % (newfilename, strftime('%y%m%d%H%M%S', gmtime()))
         from shutil import copyfile
-        copyfile(newfilename, newfilename + '.back')
+        copyfile(newfilename, backup_name)
+
     newfile = open(newfilename, 'w')
     newfile.write(ET.tostring(urdf.getroot()))
     newfile.close()
